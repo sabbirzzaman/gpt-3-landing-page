04957b85afe1d0e380b23d8ea2f6c89f88f50708
sabbirzzaman <sztonmoy7@gmail.com>
2022-03-14 02:24:02 +0600

    mobile navigation added

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+// Select elements
+const openBar = document.getElementById('ber-open');
+const closeBar = document.getElementById('bar-close');
+const navContainer = document.getElementById('mobile-nav');
+
+// Open nav bar
+openBar.addEventListener('click', () => {
+    navContainer.style.transform = 'translateX(0%)';
+    document.body.style.overflowY = 'hidden';
+})
+
+// Close nav bar
+closeBar.addEventListener('click', () => {
+    navContainer.style.transform = 'translateX(-100%)';
+    document.body.style.overflowY = 'scroll';
+})
